46a170e13d06a7c8103fc97e47f6ffdedfd75c1d
Kush21singh <ankush76728@gmail.com>
2023-09-20 20:08:16 +0530

    addExpenseHandler

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import NewExpense from './comp/Expense/NewExpense/NewExpense';
+import ExpenseItem from './comp/Expense/ExpenseItems';
+
+function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: 'General Store',
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: 'TV Showroom',
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: 'Car Showroom',
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: 'Home Decor',
+    },
+  ]);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
